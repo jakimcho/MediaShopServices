f81c7c425f568756ded5ebdcc50fe50bf15b56a0
Yakim H. Rachev <jakimcho@gmail.com>
2018-11-03 15:30:01 +0200

    added some middelware exception checks

--- routes/genres.js
+++ routes/genres.js
@@ -1,16 +1,23 @@
 const _ = require( 'lodash' );
+const asyncMiddleware = require( '../midleware/async' );
 const express = require( 'express' );
 const auth = require( '../midleware/auth' );
 const { Genre, genreSchema } = require( '../models/genre' );
 
 const router = express.Router();
 
-router.post( '/', auth, async ( req, res ) =>
+router.get( '/', asyncMiddleware( async (req, res ) => 
+{
+  const genres = await Genre.find().sort( 'name' );
+  res.send( genres );
+}));
+
+router.post( '/', auth, asyncMiddleware( async ( req, res ) =>
   { 
     const genre = new Genre( _.pick(req.body, [ "name" ] ) );
     await genre.save();
 
     res.send( genre );
-  } );  
+  } ));  
 
   module.exports = router;
